f132f5c6bd7ff6d5c8a22039403bfdac962c2500
Skylar-Lorena <lorayne911@gmail.com>
2023-01-12 17:40:04 +0300

    demo if else statement

--- control flows/demo.py
+++ control flows/demo.py
@@ -2,3 +2,20 @@
 height = 157  
 if height > 160:
     print("You are really short")
+
+# > Greater than
+# < Less than
+# ==Equal to
+# !=not equal to
+# >= Greater than or equal to
+# <=Less than or Equal to
+# and Checks if both conditions evaluate to true
+# or Checks if at least one condition is true
+# not returns the opposite of the condition given
+# The else statement comes right at the end of the if statement. 
+# It is run only when the if statement evaluates to False
+height = 199 
+if height < 160:
+    print("You are really short")
+else:
+    print("You are really tall")
