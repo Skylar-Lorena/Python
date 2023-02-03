bb1d8a777272686a5c5529f3d610754b66ee5b66
Skylar-Lorena <lorayne911@gmail.com>
2023-02-03 11:29:19 +0300

    execute code using for loop

---
+++ control flows/loop.py
@@ -0,0 +1,6 @@
+# Looping
+# A loop is a way to execute some piece of code over and over again.
+# A for loop is used when you want to repeat something a number of times. 
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
