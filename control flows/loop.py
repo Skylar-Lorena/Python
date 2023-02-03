67a02c191d107df627781b6231d3bc042cb4eebc
Skylar-Lorena <lorayne911@gmail.com>
2023-02-03 12:00:59 +0300

    execute code using while loop

--- control flows/loop.py
+++ control flows/loop.py
@@ -13,3 +13,11 @@ for number in range(1,7):
     print(number)
 # The range function takes up two parameters: start and end
 
+#While
+# A while loop is a way to execute some piece of code while a condition is true.
+students = 33
+while students >0:
+    print (students)
+    students -= 1
+
+    
