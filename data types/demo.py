62966e695c00ba72da55b7125756b467e53ede79
Skylar-Lorena <lorayne911@gmail.com>
2023-02-03 12:20:15 +0300

    create and join list using extend method

---
+++ data types/demo.py
@@ -0,0 +1,9 @@
+# lists are the equivalent of arrays in other languages
+# we can create a list with square brackets or parentheses
+
+list_a= ()
+list_b = [1,2,3,4,5]
+list_c = ["Emily",2,3,4,"windows"]
+#we can join lists using the extend method
+list_b.extend(list_c)
+print(list_b)
