fadcfadbb53b04d48b88df38cd9064ad2a553c3c
Skylar-Lorena <lorayne911@gmail.com>
2023-02-03 12:38:04 +0300

    use reverse and sort to arrange list items

--- data types/demo.py
+++ data types/demo.py
@@ -15,3 +15,10 @@ list_b.append("Wakanyi")
 print(list_b)
 list_a.append("Wakanyi")
 print(list_a)
+
+# we can use the reverse method to reverse the list and use sort to arrange items in ascending order
+list_b.reverse()
+print(list_b)
+list_d = [99,28, 22, 768, 100]
+list_d.sort()
+print(list_d)
