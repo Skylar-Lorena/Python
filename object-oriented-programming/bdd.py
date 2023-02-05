0f04cfe92042e12c7630c7398101a3b0ca5f19c4
Skylar-Lorena <lorayne911@gmail.com>
2023-02-05 15:41:50 +0300

    define bdd and its implementation

---
+++ object-oriented-programming/bdd.py
@@ -0,0 +1,12 @@
+# Behavior Driven Development
+# It is a process that facilitates the collaboration between the product owner, tester 
+# and developer when determining the system behavior.
+# BDD allows the development team to have a clear understanding of the uncertainties, which
+# in turn helps them create a product that matters. It focuses on why the features are created rather than how.
+# Implementing BDD
+# Given : Daisy and John have email accounts.
+# When: John opens his email.
+# Then: He should see the mail from Daisy.
+# We use BDD to facilitate good communication between the different parties that are involved when developing a product. 
+# We are able to better understand everything that is required when developing the system, through constant feedback and use of examples.
+
