3e98b72c16b815de60c29d9636ae7a713b59c986
Skylar-Lorena <lorayne911@gmail.com>
2023-02-05 15:02:11 +0300

    Reflection questions to review after coursework

---
+++ python-basic-exercises/theory.py
@@ -0,0 +1,16 @@
+# You can review this after the course work to check for understanding 
+
+# Questions
+# What is the Python Shell used for ?
+
+# What are the primary Data types in Python ?
+
+# What is the difference between a list,tuple and dictionary ?
+
+# What is slicing ?
+
+# Why is exception handling useful ?
+
+# What is the difference between a Python module and Python package ?
+
+# What are some of the most common programmer caused errors, and what do they mean ?
