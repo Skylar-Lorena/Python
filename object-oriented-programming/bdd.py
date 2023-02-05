17fe5bfa5337dde5580f5c84aab7d162ab984b97
Skylar-Lorena <lorayne911@gmail.com>
2023-02-05 15:44:56 +0300

    define tdd and its implementation

--- object-oriented-programming/bdd.py
+++ object-oriented-programming/bdd.py
@@ -10,3 +10,14 @@
 # We use BDD to facilitate good communication between the different parties that are involved when developing a product. 
 # We are able to better understand everything that is required when developing the system, through constant feedback and use of examples.
 
+# Test Driven Development/Design
+# TDD involvees Coding, Testing and Refactoring.
+# It can be summarized into the following steps:
+# Write a single test describing a feature of the program.
+# Run the test and to make sure it fails because your program lacks that feature.
+# Write the simplest code possible to make test pass.
+# Refactoring the code till it confirms to the simplest criteria.
+# Benefits of TDD
+# Fewer bugs in code -because you have tested out the main features
+# Focus - your focus is narrowed to making sure the test passes
+# Documentation - tests act like documentation in that if you do not know what a bit of code does you can read it and see what it does.
